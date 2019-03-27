671d2c8af1d53cb9e187639e4318ac9f91ff7ea5
Adrian Słowiński <adrian.slowinski87@gmail.com>
2019-03-27 12:51:25 +0100

    Add components

---
+++ src/components/Result.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Result = (props) => {
+  return ( 
+    <form>
+      <input type="text"/>
+      <button type="submit">Wyszukaj miasta</button> 
+    </form>
+   );
+}
+ 
+export default Result;
