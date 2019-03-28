e2a8ec1eb2932bc538001a215090abc579c76f0b
Adrian Słowiński <adrian.slowinski87@gmail.com>
2019-03-28 01:42:41 +0100

    Temperature result reafactor

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,16 +2,33 @@ import React from 'react';
 
 const Result = (props) => {
   const {err, city, date, temp, sunset, sunrise, pressure, wind} = props.weather
+
+  let content = null
+
+  if(!err && city) {
+
+    const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
+    const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
+    content = (
+      <div>
+        <h2>  {`Dane pogodowe dla miasta ${city}:`} </h2>
+        <div> Dzień: {date}</div>
+        <div> Temperatura: {temp} &#176;C</div>
+        <div> Wschód: {sunriseTime}</div>
+        <div> Zachód: {sunsetTime}</div>
+        <div> Wiatr: {wind} m/s</div>
+        <div> Ciśnienie: {pressure} hPa</div>
+      </div>
+    )
+  }
+
   return ( 
-    <React.Fragment>
-      <div> Pogoda dla: {city} </div>
-      <div> Dzień: {date}</div>
-      <div> Temperatura: {temp}</div>
-      <div> Wschód: {sunrise}</div>
-      <div> Zachód: {sunset}</div>
-      <div> Ciśnienie: {pressure}</div>
-      <div> Wiatr: {wind}</div>
-    </React.Fragment>
+    <div className="result">
+      {err ? `Brak ${city} w bazie danych` : content}
+    </div>
+    // <React.Fragment>
+    //   <div> Pogoda dla: {city} </div>
+    // </React.Fragment>
    );
 }
  
